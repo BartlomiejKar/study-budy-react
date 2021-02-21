af6d6163b8fc8278957dd3de95b0050a0fdbb683
Bartek Karelus <bartek.karelus@gmail.com>
2021-02-21 19:24:55 +0100

    add hooks useState and UseEffect, add isLoading screen

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { users } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { StyledList, Wrapper } from './UsersList.styles';
 
-const UsersList = () => (
-  <Wrapper>
-    <StyledList>
-      {users.map((userData, index) => (
-        <UsersListItem index={index} key={userData.name} userData={userData} />
-      ))}
-    </StyledList>
-  </Wrapper>
-);
+const UsersList = () => {
+  const [user, setUser] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setUser(users);
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  const UserList = user.map((userData, index) => <UsersListItem index={index} key={userData.name} userData={userData} />);
+  return (
+    <Wrapper>
+      <StyledList>{isLoading ? <h1>is Loading...</h1> : UserList}</StyledList>
+    </Wrapper>
+  );
+};
 
 export default UsersList;
