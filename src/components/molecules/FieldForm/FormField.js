0d42855532a8936c329d9ae531af19c0595c7cb7
Bartek Karelus <bartek.karelus@gmail.com>
2021-02-27 21:51:48 +0100

    add style for Container in FormField.

--- src/components/molecules/FieldForm/FormField.js
+++ src/components/molecules/FieldForm/FormField.js
@@ -2,23 +2,28 @@ import React from 'react';
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import { Label } from "components/atoms/Label/Label";
-import { Input } from "components/atoms/Input/Input"
+import { Input } from "components/atoms/Input/Input";
 
 
-const Wrapper = styled.div`
-background-color: yellow;
+const Container = styled.div`
+display: flex;
+flex-direction:column;
+align-items: flex-start;
+${Label} {
+   margin: 15px 0;
+}
 `;
 
 const FormField = ({ label, type = "text", name, id, ...props }) => {
     return (
-        <>
+        <Container>
             <Label htmlFor={id}>{label}</Label>
             <Input name={name} id={id} type={type} />
-        </>
-    )
+        </Container>
+    );
 };
 
-FormField.PropTypes = {
+FormField.propTypes = {
     type: PropTypes.string,
     name: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired,
